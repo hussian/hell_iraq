62b147c1cf35af5e51a085bb07465d132c8d5494
hussian <jauzmzmm1@moakt.ws>
2016-08-25 15:44:49 -0500

    Add files via upload

---
+++ plugins/meee.lua
@@ -0,0 +1,44 @@
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀     BY(@AHMED_ALOBIDE)               ▀▄ ▄▀ 
+▀▄ ▄▀      BY(@hussian_9)                  ▀▄ ▄▀ 
+▀▄ ▄▀                                      ▀▄ ▄▀   
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+do
+
+local function oscar(msg ,matches)
+        if is_sudo(msg) then
+local url = "http://e.top4top.net/p_225843h1.jpg"
+local mustafadev = download_to_file(url,'DeaD.jpg')
+  send_photo(get_receiver(msg),mustafadev,ok_cb,false)
+
+        elseif is_owner(msg) then
+        local url = "http://f.top4top.net/p_2252bs01.jpg"
+local mustafadev = download_to_file(url,'DeaD.jpg')
+
+      send_photo(get_receiver(msg),mustafadev,ok_cb,false)
+        elseif is_momod(msg) then
+        local url = "http://f.top4top.net/p_225lowe1.jpg"
+local mustafadev = download_to_file(url,'DeaD.jpg')
+
+   send_photo(get_receiver(msg),mustafadev,ok_cb,false)
+        elseif not is_momod(msg) then
+        local url = "http://b.top4top.net/p_225uov51.jpg"
+local mustafadev = download_to_file(url,'DeaD.jpg')
+
+     send_photo(get_receiver(msg),mustafadev,ok_cb,false)
+        end
+end
+
+return { 
+    patterns = { 
+        "^(me)$"
+    },
+    run = AHMED_ALOBIDE,
+    
+}
+--by @AHMED_ALOBAIDE
+end
